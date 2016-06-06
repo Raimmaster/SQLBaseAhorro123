86ad1f16b023f95345e48ca2249fb7dbd3e1cfdb
Raim Interiano <rai.interiano@gmail.com>
2016-06-05 18:34:17 -0600

    SQL Procedures generated from self-made app

--- Stored Procedures - SQLBase.sql
+++ Stored Procedures - SQLBase.sql
@@ -15,48 +15,113 @@ Actions
 	On Procedure Close
 		Call SqlDisconnect( hSql )
 
-------
-STORE SP_INSERT_USUARIOS
-PROCEDURE: SP_INSERT_USUARIOS static
-Parameters
-	STRING: vID_USER
-	STRING: vUSERNAME
-	STRING: vCLAVE
-	DATE/Time:	FECHA_CREACION 
-	DATE/Time:	FECHA_MODIFICACION
-	STRING: vID_USER_CREADOR
-	STRING: vID_USER_MODIFICADOR
-Local Variables
-	Sql Handle hSqlProc
-Actions
-	On Procedure Startup
-		Call SqlConnect (hSqlProc)
-	On Procedure Execute
-		Call SqlPrepare( hSqlProc, \
-			'INSERT INTO 
-			USUARIOS (ID_USUARIO, USERNAME, CLAVE_USUARIO, 
-			FECHA_CREACION_USU, FECHA_ACTUALIZACION_USU,
-			CODIGO_USUARIO_CREADOR, CODIGO_USUARIO_ACTUALIZO)
-			 VALUES (:vID_USER, :vUSERNAME, :vCLAVE, 
-			:FECHA_CREACION, :FECHA_MODIFICACION, 
-			:vID_USER_CREADOR, :vID_USER_MODIFICADOR);')
-		Call SqlExecute(hSqlProc)
-	On Procedure Close
-		Call SqlDisconnect( hSqlProc );
-
 ------Sí funciona
-Procedure USUARIOS_INSERT STATIC
+--------USUARIOS
+STORE SP_USUARIOS_INSERT
+Procedure SP_USUARIOS_INSERT STATIC
 Parameters
-	STRING  n_Id_Usuario
-	STRING  n_Clave
-	STRING  n_Username
+	STRING:  n_Id_Usuario
+	STRING:  n_Clave
+	STRING:  n_Username
 Local Variables
 	Sql Handle  hSql
 Actions
 	On Procedure Startup
 		Call SqlConnect( hSql )
 	On Procedure Execute
-		Call SqlPrepare( hSql, 'INSERT INTO USUARIOS (ID_USUARIO, USERNAME, CLAVE_USUARIO) VALUES (n_Id_Usuario, n_Username, n_Clave)' )
+		Call SqlPrepare( hSql, 'INSERT INTO USUARIOS (ID_USUARIO, USERNAME, CLAVE_USUARIO) VALUES (:n_Id_Usuario, :n_Username, :n_Clave)' )
 		Call SqlExecute( hSql )
 	On Procedure Close
-		Call SqlDisconnect( hSql );	
+		Call SqlDisconnect( hSql );
+
+-------
+STORE SP_USUARIOS_UPDATE
+Procedure SP_USUARIOS_UPDATE STATIC
+Parameters
+	STRING:  n_Id_Usuario
+	STRING:  n_Clave
+	STRING:  n_Username
+Local Variables
+	Sql Handle  hSql
+Actions
+	On Procedure Startup
+		Call SqlConnect( hSql )
+	On Procedure Execute
+		Call SqlPrepare( hSql, 'UPDATE USUARIOS SET USERNAME = :n_Username, CLAVE_USUARIO = :n_Clave WHERE ID_USUARIO = :n_Id_Usuario' )
+		Call SqlExecute( hSql )
+	On Procedure Close
+		Call SqlDisconnect( hSql );
+
+--------------
+STORE SP_USUARIOS_DELETE
+Procedure SP_USUARIOS_DELETE STATIC
+Parameters
+	STRING:  n_Id_Usuario
+Local Variables
+	Sql Handle  hSql
+Actions
+	On Procedure Startup
+		Call SqlConnect( hSql )
+	On Procedure Execute
+		Call SqlPrepare( hSql, 'DELETE FROM USUARIOS WHERE ID_USUARIO = :n_Id_Usuario' )
+		Call SqlExecute( hSql )
+	On Procedure Close
+		Call SqlDisconnect( hSql );
+
+-----------------CUENTAS
+-------
+STORE SP_CUENTAS_INSERT
+Procedure SP_CUENTAS_INSERT STATIC
+Parameters
+	NUMBER: n_Num_Cuenta
+	DATE/TIME: n_Fecha_Apert
+	NUMBER: n_Saldo
+	STRING: n_Tipo_Cuenta
+Local Variables
+	Sql Handle  hSql
+Actions
+	On Procedure Startup
+		Call SqlConnect( hSql )
+	On Procedure Execute
+		Call SqlPrepare( hSql, 'INSERT INTO CUENTAS(NUMERO_CUENTA, FECHA_APERTURA, SALDO_CUENTA, TIPO_CUENTA) VALUES (:n_Num_Cuenta, :n_Fecha_Apert, :n_Saldo, :n_Tipo_Cuenta)' )
+		Call SqlExecute( hSql )
+	On Procedure Close
+		Call SqlDisconnect( hSql );
+
+-------
+STORE SP_CUENTAS_UPDATE
+Procedure SP_CUENTAS_UPDATE STATIC
+Parameters
+	NUMBER: n_Num_Cuenta
+	DATE/TIME: n_Fecha_Apert
+	NUMBER: n_Saldo
+	STRING: n_Tipo_Cuenta
+Local Variables
+	Sql Handle hSql
+Actions
+	On Procedure Startup
+		Call SqlConnect( hSql )
+	On Procedure Execute
+		Call SqlPrepare( hSql, 'UPDATE CUENTAS SET FECHA_APERTURA = n_Fecha_Apert, SALDO_CUENTA = :n_Saldo, TIPO_CUENTA = :n_Tipo_Cuenta WHERE NUMERO_CUENTA = :n_Num_Cuenta' )
+		Call SqlExecute( hSql )
+	On Procedure Close
+		Call SqlDisconnect( hSql );
+
+-------
+STORE SP_CUENTAS_DELETE
+Procedure SP_CUENTAS_DELETE STATIC
+Parameters
+	NUMBER: n_Num_Cuenta
+Local Variables
+	Sql Handle hSql
+Actions
+	On Procedure Startup
+		Call SqlConnect( hSql )
+	On Procedure Execute
+		Call SqlPrepare( hSql, 'DELETE FROM CUENTAS WHERE NUMERO_CUENTA = :n_Num_Cuenta' )
+		Call SqlExecute( hSql )
+	On Procedure Close
+		Call SqlDisconnect( hSql );
+
+---------------------
+-----ABONOS
